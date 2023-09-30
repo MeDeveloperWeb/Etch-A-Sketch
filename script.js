f2d5d07d869a1b081afb8512cffdff5f3cdfc02a
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2023-09-30 15:27:51 +0530

    Creates Grid with the help of slider and input box

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-const sketchpad = document.querySelector(".sketchpad");
+const DEFAULT_GRID_SIZE = 50;
 
-function createGrids(size) {
+const sketchpad = document.querySelector(".sketchpad");
+const slider = document.querySelector(".slider");
+
+// HTML has span first and Input later
+const [gridColSizeSpan, gridRowSizeInput] = document.querySelectorAll(".grid-size");
+
+/**
+ * Creates grid on chosen size
+ * @param {number} gridSize
+ * @argument e.target.value
+ */
+function createGrids(gridSize) {
+
     const gridRow = document.createElement('div');
     gridRow.setAttribute("class", "grid-row");
 
     const gridCell = document.createElement('div');
     gridCell.setAttribute("class", "grid-cell");
 
-    for (let i=0; i < size; i++) {
+    for (let i=0; i < gridSize; i++) {
         gridRow.appendChild(gridCell.cloneNode(true)); 
     }
-    for (let i=0; i < size; i++) {
+    for (let i=0; i < gridSize; i++) {
         sketchpad.appendChild(gridRow.cloneNode(true));
     }
 }
 
-document.querySelector("button").onclick = () => {
+/**
+ * Changes value of span.grid-size and slider so that they all are in sync
+ * Dispatches change event in slider
+ */
+gridRowSizeInput.addEventListener("input", () => {
+
+    if (gridRowSizeInput.value >= 100) gridRowSizeInput.value = 100;
+
+    if (!gridRowSizeInput.checkValidity()) {
+        return gridRowSizeInput.reportValidity();
+    }
+    // Changes value of span and slider so that they are in sync
+    gridColSizeSpan.textContent = slider.value = gridRowSizeInput.value;
+    //
+    slider.dispatchEvent(new Event('change')); 
+});
+
+/**
+ * Keeps Input box and slider in Sync
+ */
+slider.addEventListener("input", () => 
+    gridColSizeSpan.textContent = gridRowSizeInput.value = slider.value
+);
+
+/**
+ * Creates grid when value of slider changes.
+ */
+slider.addEventListener("change", (e) => {
     sketchpad.replaceChildren();
-    createGrids(+prompt("grid"));
-}
+    createGrids(+e.target.value);
+});
+
+// Create grids by default
+createGrids(DEFAULT_GRID_SIZE);
