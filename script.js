79cbad0f0d75291a4b63f585ab46f3eab23e9be6
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2023-10-02 18:01:46 +0530

    Using Svg Images

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const DEFAULT_GRID_SIZE = 50;
 const sketchpad = document.querySelector(".sketchpad");
 const slider = document.querySelector(".slider");
 
-let sketchPadColor = "#ffffff"; 
+let currentGridSize;
+
 let sketchPadPenColor = "#000000";
 let sketchMode = "pen";
 let pixelDrawn = 0;
@@ -13,6 +14,18 @@ const [gridColSizeSpan, gridRowSizeInput] = document.querySelectorAll(".grid-siz
 
 // var to store if drawing is allowed on sketch board
 let isDrawingEnabled = false;
+
+/**
+ * Sets the width of The sketchpad elements
+ */
+function setSketchPadDimensions() {
+    const deviceHeight = window.innerHeight;
+    const deviceWidth = window.innerWidth;
+
+    const sketchpadToolCont = document.querySelector(".tools-container");
+
+    sketchpad.style.width = sketchpad.style.height = sketchpadToolCont.style.width = Math.min(deviceHeight*8/10, deviceWidth*9/10, 960) + 'px';
+}
 
 /**
  * Creates grid on chosen size
@@ -37,20 +50,33 @@ function createNewGrids(gridSize) {
     }
 }
 
+/**
+ * Changes the background color of provided Pixels
+ * @param {HTMLElement} pixelDiv 
+ * @param {string} mode {"pen", "brush", "eraser", "random"}
+ */
 function draw(pixelDiv, mode) {
     if (mode === "pen") pixelDiv.style.backgroundColor = sketchPadPenColor;
-    else if (mode === "eraser") pixelDiv.style.removeProperty("background-color");
-    else if (mode === "random") pixelDiv.style.backgroundColor = '#' + Math.random().toString(16).slice(-6);
-    else if (mode === "brush") {
 
-        let opacity = pixelDrawn < 200 ? 255 - pixelDrawn : 55;
+    else if (mode === "eraser") pixelDiv.style.removeProperty("background-color");
+
+    // For selecting random colors.
+    else if (mode === "random") pixelDiv.style.backgroundColor = '#' + Math.random().toString(16).slice(-6);
+
+    else if (mode === "brush") {
+        // Decreasing the opacity of the brush as soon as pixelDrawn Increases
+        // Opacity decreases according to the currentGridSize
+        // For pixelDrawn 0, Opacity is maximum, for pixelDrawn >= currentGridSize opacity is minimum
+        // For the middle values we are getting the equivalent value of it, if it were divided in 256 intervals.
+        let opacity = Math.floor(((currentGridSize) - pixelDrawn) * 255 / (currentGridSize));
+        if (opacity < 50) opacity = 50
 
         if (pixelDiv.style.backgroundColor) {
             let currentRGB = pixelDiv.style.backgroundColor
-            let currentOpacity = currentRGB.slice(currentRGB.lastIndexOf(',')+2, -1);
-
-            opacity += Math.round(+currentOpacity * 255);
-            console.log(opacity);
+            // We are getting opacity value in terms of RGB
+            let currentOpacity = currentRGB.slice(currentRGB.lastIndexOf(',') + 2, -1);
+            // If the pixel has already been drawn we don't want to overlay that with less opacity color
+            opacity = Math.max(+currentOpacity*255, opacity);
             if (opacity > 256) opacity = 255;
         }
 
@@ -58,11 +84,7 @@ function draw(pixelDiv, mode) {
         pixelDiv.style.backgroundColor = sketchPadPenColor + hexOpacity;
         pixelDrawn++;
     }
-    else if (mode = "move") return;
 }
-
-// Create grids by default
-createNewGrids(DEFAULT_GRID_SIZE);
 
 /**
  * Handles all grid manipulation event listeners
@@ -96,55 +118,70 @@ function gridManipulatorListeners() {
      * Creates grid when value of slider changes.
      */
     slider.addEventListener("change", (e) => {
-        createNewGrids(+e.target.value);
+        createNewGrids(currentGridSize = +e.target.value);
     });
 
     // Toggle b/w showing and hiding grid lines
-    document.querySelector("#grid-lines-checkbox").addEventListener("change", (e) => {
-        sketchpad.classList.toggle("sketch-pad-with-grid")
+    document.querySelector("#grid-lines-checkbox").addEventListener("click", (e) => {
+        sketchpad.classList.toggle("sketch-pad-with-grid");
+        if (e.target.src.slice(-7) === "eye.svg") e.target.src = "media/eye-off.svg"
+        else e.target.src = "media/eye.svg";
     })
 }
 
-gridManipulatorListeners();
-
-
+/**
+ * Handles all grid drawing event listeners
+ */
 function gridDrawingListeners() {
     // Enable Drawing, If clicked on sketch board
-    sketchpad.addEventListener("pointerdown", (e) => {
-        if (e.target.className.includes("grid-cell")) draw(e.target, sketchMode)
+    sketchpad.addEventListener("mousedown", (e) => {
+        /**
+         * Why not pointer down?
+         * To enable 2 finger pinch zoom.
+         * How does it enable that?
+         * For some reason this event triggers for short duration touches only
+         * (?? It is the property maybe??)
+         */
+        if (e.target.className.includes("grid-cell")) draw(e.target, sketchMode);
         isDrawingEnabled = true;
     });
 
     // Disable Drawing if mouse or finger removed
-    document.addEventListener("pointerup", () => {
+    document.addEventListener("pointerup", (e) => {
         isDrawingEnabled = false;
         pixelDrawn = 0;
     });
 
-    // Drawing when pointer moves
-    sketchpad.addEventListener("pointermove", (e) => {
-        let targetPixel;
+    // Disable drawing in case of pointer cancel
+    sketchpad.addEventListener("pointercancel", (e) => {
+        isDrawingEnabled = false;
+    });
 
-        /**
-         * For mobile devices
-         * Mobile pointer event don't exactly work like computer's. When using pointer over 
-         * I was able to get only that div.grid-cell where I touched in the beginning in e.target
-         * To solve this, I am getting the div.grid-cell from current finger position.
-         * Thanks to this video: https://www.youtube.com/watch?v=MhUCYR9Tb9c
+    // Draw with touch
+    sketchpad.addEventListener("touchmove", (e) => {
+         /**
+         * For mobile devices,
+         * Not using pointer move as there is not a simple way of getting the no. of fingers (none that I can find)
+         * I need finger count to enable two pinch zoom
+         * I am getting the div.grid-cell from current finger position.
+         * Thanks to this video: https://www.youtube.com/watch?v=MhUCYR9Tb9c (Used this originally for pointer, still it helped)
          */
-        if (e.pointerType === "touch") targetPixel = document.elementFromPoint(e.pageX, e.pageY);
-        /**
-         * Else for computer devices, 
-         * e.target works completely fine.
-         */
-        else targetPixel = e.target;
+        if (e.touches.length > 1) return;
+        let touch = e.touches[0];
+        let targetPixel = document.elementFromPoint(touch.pageX, touch.pageY);
+        draw(targetPixel, sketchMode);
 
-        if (isDrawingEnabled && targetPixel.className.includes("grid-cell")) draw(targetPixel, sketchMode)
+    })
+
+    //Drawing when mouse moves
+    sketchpad.addEventListener("mousemove", (e) => {
+        if (isDrawingEnabled && e.target.className.includes("grid-cell")) draw(e.target, sketchMode);
     });
 }
 
-gridDrawingListeners();
-
+/**
+ * Handles click on the sketchpad tools
+ */
 function sketchToolListeners() {
     /**
      * @param {string} hexColor hex value of color
@@ -153,15 +190,17 @@ function sketchToolListeners() {
     function isDarkHexColor(hexColor) {
         // Takes the 2nd character of hex color string if it's NaN it's already greater than 9
         // Red part of color tells enough about the darkness of color (at least for my case)
-        if (+hexColor[1] !== NaN && +hexColor[1] < 5) return true;
+        if (hexColor[1] < "8") return true;
         return false;
     }
 
     const toolBar = document.querySelector(".tools-container");
 
+    // Color of the pen or brush
     const colorPicker = toolBar.querySelector("#color-picker");
     const colorPickIcon = toolBar.querySelector("#color-select");
 
+    // Background color of the sketchpad
     const bgColorPicker = toolBar.querySelector("#bg-color-picker");
     const bgColorPickIcon = toolBar.querySelector("#bg-color-select");
 
@@ -171,30 +210,49 @@ function sketchToolListeners() {
     bgColorPickIcon.onclick = (e) => bgColorPicker.click();
 
     // Color picker to select the pen color
-    colorPicker.addEventListener('change', (e) => {
-        colorPickIcon.style.backgroundColor = colorPicker.value;
-        sketchPadPenColor = colorPicker.value;
+    colorPicker.addEventListener('change', () => {
+        //Changing the Icon Color too along with the pen color
+        colorPickIcon.style.backgroundColor = sketchPadPenColor = colorPicker.value;
+
+        document.querySelector('body').style.backgroundColor = colorPicker.value + "80"
+
+        // Change The value of logo if it's visible
+        // Because Header is inheriting body's background color
+        const header = document.querySelector('header');
+        if (isDarkHexColor(colorPicker.value)) header.style.color = "white";
+        else header.style.color = "rgba(64, 64, 64, 0.8)";
+
     });
 
     // Color Picker to select the Background color of sketch board
-    bgColorPicker.addEventListener('change', (e) => {
-        sketchPadColor = bgColorPicker.value;
-        sketchpad.style.backgroundColor = sketchPadColor;
+    bgColorPicker.addEventListener('change', () => {
+        sketchpad.style.backgroundColor = bgColorPicker.value;
 
         // To make sure grid lines are visible
-        if (isDarkHexColor(sketchPadColor)) sketchpad.classList.add("dark-bg");
+        if (isDarkHexColor(bgColorPicker.value)) sketchpad.classList.add("dark-bg");
         else sketchpad.classList.remove("dark-bg");
     });
 
     toolBar.querySelectorAll(".tool").forEach(el => {
-        if (el.id.length < 7) el.onclick = () => {
-            sketchMode = el.id;
-            if (el.id === "move") sketchpad.style.touchAction = "auto";
-            else sketchpad.style.touchAction = "pinch-zoom";
-        }
+        // I have used class names with length less than 7 for pen, brush, random color and eraser
+        if (el.id.length < 7) el.onclick = () => sketchMode = el.id;
     });
 
     toolBar.querySelector("#delete-all").onclick = () => createNewGrids(slider.value);
 }
+/**
+ * Set SketchPad Dimensions in Pixel
+ * I know it's probably the bad idea to use this in js
+ * But zoom does not work properly while using viewport sizes in css.
+ */
+setSketchPadDimensions();
 
+// Create grids by default
+createNewGrids(currentGridSize=DEFAULT_GRID_SIZE);
+
+// Bind all required elements with event listeners
+gridDrawingListeners();
+gridManipulatorListeners();
 sketchToolListeners();
+
+
